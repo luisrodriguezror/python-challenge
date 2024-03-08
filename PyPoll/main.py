dd4f276026f24bcefea57f0e02c4b4f505b0bc27
luisrodriguezror <luisrodriguezro@hotmail.com>
2024-03-07 21:21:13 -0600

    final change

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -24,17 +24,45 @@ with open(csvpath) as csvfile:
             candidates.append(names)
 
 
-print(candidates)
-
-
+winpercentage=0
+winner=""
 
 print(f'''Election Results
 ---------------------------------
-Total Votes: {numbervotes}
+Total Votes: {numbervotes:,}
 ---------------------------------''')
 for candidate in candidates:
     candidate_votes = sum(1 for row in csv.reader(open(csvpath)) if row[2] == candidate)
     percentage = (candidate_votes / numbervotes) * 100
-    print(f"{candidate}: {percentage:.3f}% ({candidate_votes})")
+    print(f"{candidate}: {percentage:.2f}% ({candidate_votes:,})")
 
-##
+    if percentage > winpercentage:
+        winpercentage=percentage
+        winner=candidate
+print('---------------------------------')
+print(f'''Winner: {winner}
+---------------------------------''')
+
+output_path = os.path.join("PyPoll", "analysis", "analysis_result.txt")
+
+output_path = os.path.join("PyPoll", "analysis", "analysis_result.txt")
+
+with open(output_path, "w") as file:
+    file.write(f'''Election Results
+---------------------------------
+Total Votes: {numbervotes:,}
+---------------------------------
+''')
+    for candidate in candidates:
+        candidate_votes = sum(1 for row in csv.reader(open(csvpath)) if row[2] == candidate)
+        percentage = (candidate_votes / numbervotes) * 100
+        file.write(f'''{candidate}: {percentage:.2f}% ({candidate_votes:,})
+''')
+
+        if percentage > winpercentage:
+            winpercentage=percentage
+            winner=candidate
+    file.write('''---------------------------------
+''')
+    file.write(f'''Winner: {winner}
+---------------------------------''')
