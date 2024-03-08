ad0046b81137928c669cbba030a72f0e4952c8d0
luisrodriguezror <luisrodriguezro@hotmail.com>
2024-03-07 21:48:24 -0600

    final hwk

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,68 +1,74 @@
 import os
 import csv
 
-csvpath = os.path.join('PyPoll','Resources', 'election_data.csv')
+csvpath = os.path.join('PyPoll', 'Resources', 'election_data.csv')
 
-numbervotes=0
-candidates = []
-winner= ""
-
-
+numbervotes = 0
+candidates = {}
+winner = ""
 
 with open(csvpath) as csvfile:
-    csvreader=csv.reader(csvfile,delimiter=",")
+    csvreader = csv.reader(csvfile, delimiter=",")
     next(csvreader)
 
-
     for row in csvreader:
-        numbervotes+=1
-        names = row[2]
+        numbervotes += 1
+        candidate_name = row[2]
 
-        if names in candidates:
-            continue
+        if candidate_name in candidates:
+            candidates[candidate_name] += 1
         else:
-            candidates.append(names)
+            candidates[candidate_name] = 1
 
-
-winpercentage=0
-winner=""
+winpercentage = 0
+winner = ""
 
 print(f'''Election Results
 ---------------------------------
 Total Votes: {numbervotes:,}
 ---------------------------------''')
-for candidate in candidates:
-    candidate_votes = sum(1 for row in csv.reader(open(csvpath)) if row[2] == candidate)
-    percentage = (candidate_votes / numbervotes) * 100
-    print(f"{candidate}: {percentage:.2f}% ({candidate_votes:,})")
+
+for candidate, votes in candidates.items():
+    percentage = (votes / numbervotes) * 100
+    print(f"{candidate}: {percentage:.2f}% ({votes})")
 
     if percentage > winpercentage:
-        winpercentage=percentage
-        winner=candidate
+        winpercentage = percentage
+        winner = candidate
+
 print('---------------------------------')
-print(f'''Winner: {winner}
----------------------------------''')
+print(f'Winner: {winner}')
+print('---------------------------------')
+
 
 output_path = os.path.join("PyPoll", "analysis", "analysis_result.txt")
 
-output_path = os.path.join("PyPoll", "analysis", "analysis_result.txt")
 
 with open(output_path, "w") as file:
     file.write(f'''Election Results
+               
 ---------------------------------
+               
 Total Votes: {numbervotes:,}
+
 ---------------------------------
-''')
-    for candidate in candidates:
-        candidate_votes = sum(1 for row in csv.reader(open(csvpath)) if row[2] == candidate)
-        percentage = (candidate_votes / numbervotes) * 100
-        file.write(f'''{candidate}: {percentage:.2f}% ({candidate_votes:,})
 ''')
 
-        if percentage > winpercentage:
-            winpercentage=percentage
-            winner=candidate
-    file.write('''---------------------------------
+    for candidate, votes in candidates.items():
+        percentage = (votes / numbervotes) * 100
+        file.write(f'''
+{candidate}: {percentage:.2f}% ({votes})
 ''')
-    file.write(f'''Winner: {winner}
+
+    if percentage > winpercentage:
+        winpercentage = percentage
+        winner = candidate
+
+    file.write('''
 ---------------------------------''')
+    file.write(f'''
+
+Winner: {winner}
+
+''')
+    file.write('---------------------------------')
